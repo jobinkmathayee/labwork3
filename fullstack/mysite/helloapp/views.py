e78769c45e439015870dfd26a965e46312cc9cbf
jobinkmathayee <137720051+jobinkmathayee@users.noreply.github.com>
2023-06-26 13:33:37 +0300

    Add files via upload

---
+++ fullstack/mysite/helloapp/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+def Hello(request):
+    return HttpResponse("hello its me jobin")
+
+# Create your views here.
---
+++ fullstack/mysite/helloapp2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def Hello(request):
+    return render(request,'tk.html')
+
+# Create your views here.
